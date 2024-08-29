9842954a5a2c6901e208daee8e5799ea06521aa3
Mhmod <mahmoud97787@gmail.com>
2024-08-29 15:39:20 +0300

    add assets

---
+++ lib/utils/assets.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  static const String note1 = "assets/note1.wav";
+
+  static const String note2 = "assets/note2.wav";
+
+  static const String note3 = "assets/note3.wav";
+
+  static const String note4 = "assets/note4.wav";
+
+  static const String note5 = "assets/note5.wav";
+
+  static const String note6 = "assets/note6.wav";
+
+  static const String note7 = "assets/note7.wav";
+}
+
